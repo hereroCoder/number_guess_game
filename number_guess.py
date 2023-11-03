cae5df6526d0a9dc1e9a5ac346028dc5c6a6b3db
Wame Chibamo <83869107+hereroCoder@users.noreply.github.com>
2023-11-03 12:28:48 +0000

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,86 +1,89 @@
 import random
-import time
-
-# number guessing game
-# user chooses to either guess the computer number
-# or Computer guess the number user is thinking
-
-def user_guess_number():
-  number = random.randint(1, 100)
-
-  count = 1
-  guess = None
-
-  print('im choosing a random number then will see if you can guess it')
-  time.sleep(3)
-  print('choosing.....hold on im shuffling')
-  time.sleep(3)
-  print('Ok got it!')
-
-  while guess != number:
-      guess = int(input('Guess a number between 1 and 100: '))
-      time.sleep(0.5)
-      if guess == number:
-          break
-      else:
-          if guess > number:
-              print('wrong, try something lower')
-          else:
-              print('wrong, try something higher')
-      count += 1
-
-  print(f'Congratulations! The correct number was {number}')
-  print(f'it took you {count} guesses')
-  # used a for loop to give the user 3 tries
-  # but changed to while loop so they can keep guessing until right
-  # introducing the time library for pauses makes this more interesting
-  # Even though it makes the program slower its adding life to it
 
 
+# computer guesses user number
 def computer_guess_number():
-  print("Let me guess the number you are thinking. I will try to do it in 10 trials")
-
-
-  print("You will give me hits if i should go lower or higher")
-  print("")
-  print("Lets Begin")
-  print("")
-  print("Think of a number between 0 and 100")
-  print("")
-
-  trials = 10
-  low = 0
-  high = 100
-
-
-  while trials > 0:
-      number = random.randint(low, high)
-      print("I guessed {}".format(number))
-      clue = input(
-          "Is that the correct number(c) or lower(l) or upper(u)").lower()
-
-      if clue == 'c':
-          print("Yay i guess the number {} correctly".format(number))
-          break
-      elif clue == 'l':
-          high = number - 1
-      elif clue == 'u':
-          low = number + 1
+  print('The computer will be guessing your number')
+  print("Think of a number")
+  print("Ok now i will try to guess it.")
+  count  = 0
+  lower_limit = 1
+  upper_limit = 100
+  computer_guess = random.randint(lower_limit,upper_limit)
+  while count < 10:
+    reponse = input (f'Is the number {computer_guess}? y/n ').lower()
+    if reponse == 'y':
+      print(f'Yay im good at this!! So your number was {computer_guess}')
+      if count <=5:
+        print(f"I think i was pretty fast, took me {count} gueses")
       else:
-          print("Sorry that was an invalid input")
-
-      trials -= 1
+        print(f"That was a tough win, took me {count} gueses")
+      break
+    elif reponse == 'n':
+      hint = input('Oh thats a bummer, should i guess higher (h) or lower(l)').lower()
+      if hint == 'l':
+        upper_limit = computer_guess
+        computer_guess = random.randint(lower_limit, upper_limit)
+      elif hint == 'h':
+        lower_limit = computer_guess
+        computer_guess = random.randint(lower_limit, upper_limit)
+      else:
+        print('Please give me a hint if i should guess higher (h) or lower (l)')
+      count += 1
+    else:
+      print('y/n did i guess the numer correctly?')
   else:
-        print("Unfortunately i didnt guess correctly after 10 trials")
+    print("Thats me giving up, i tried 10 times")
 
 
-def playing():
-  user = input("1. Do you want to guess the number 2. Do you want the computer to guess your number ")
-  if user == "1":
-    user_guess_number()
-  elif user == "2":
-    computer_guess_number()
+#user guesses computer number
+def user_guess_number():
+  print('You will be guessing the computer number')
+  print("You only have 10 chances and I will be giving you hints")
+  print("Here you go!\n")
+  number = random.randint(1,100)
+  count = 1
+  while count <= 10:
+    the_guess = int(input("Guess the number: "))
+    if the_guess == number:
+      print(f"Nice! That is correct. The number was {number}")
+      if count <=5:
+        print(f"I think i was pretty fast, took you {count} gueses")
+      else:
+        print(f"That was a tough win, took you {count} gueses")
+      break
+    elif the_guess > number:
+      print("Try something lower")
+    elif the_guess < number:
+      print("Nope, Higher")
+    count +=1
   else:
-    print("Invalid! please select 1 or 2")
+    print(f"Game over!! You lost :(). Number was {number}")
 
-playing()
+
+def game():
+  game_trials = 3
+  while game_trials > 0:
+    try:
+      print("Modes: press 1 or 2")
+      mode = int(input('which mode do you want? '))
+
+      if mode == 1:
+        computer_guess_number()
+
+      elif mode == 2:
+        user_guess_number()
+
+      else:
+        print('Sorry that was invalid,press 1 or 2 to select game mode \n')
+
+    except:
+      print('I am confused, i think you pressed an invalid character. Please try again and input numbers \n')
+
+    game_trials -= 1
+
+  else:
+    print('You can start the game again anytime')
+    print('Alright! i will see you next time')
+
+game()
